7b4957399d234e0a604ce9ef13f494d0c0b39925
Angelica <angelica1387@gmail.com>
2014-09-16 11:06:43 -0430

    Agregando archivo para declarar módulo

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -+-
+
+{
+	'name': 'Curso OpenERP',
+	'description': 'Este módulo es para aprender OpenERP',
+	'author': 'Angélica Espinosa',
+	'version': 'dia1',
+	'depends': ['base', ],
+	'data': [],
+	'demo': [],
+}
